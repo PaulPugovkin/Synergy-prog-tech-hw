515282ccf466e19a84c8bf54447f1e3093b71cc8
Paul <coastalrhodes@gmail.com>
2022-11-08 10:07:11 +0300

    add homework 3-3

--- 3_Cycles/hw-3-3/hw-3-3.cpp
+++ 3_Cycles/hw-3-3/hw-3-3.cpp
@@ -1,17 +1,9 @@
 #include <stdio.h>
 
 int main() {
-  int sum = 0;
-  int num;
+  float num;
   printf("enter num ");
-  scanf("%i", &num);
-  if (num%2 == 0) {
-    int sum2 = (num+1)*(num/2);
-    printf("Sum with algorithm: %i\n", sum2);
-  } else {
-    for (int i = 1; i<=num; i++) {
-      sum = sum+i;
-    }
-    printf("Sum with iteration: %i\n", sum);
-  }
+  scanf("%f", &num);
+  float sum = (num+1)*(num/2);
+  printf("Sum with algorithm: %f\n", sum);
 }
