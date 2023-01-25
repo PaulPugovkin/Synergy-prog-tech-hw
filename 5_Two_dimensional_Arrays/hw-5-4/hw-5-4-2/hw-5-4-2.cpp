0bd39d8be8890b46d34c63080129324860f369c8
PaulPugovkin <coastalrhodes@gmail.com>
2023-01-25 23:11:47 +0300

    edit homework 5.6.2

--- 5_Two_dimensional_Arrays/hw-5-4/hw-5-4-2/hw-5-4-2.cpp
+++ 5_Two_dimensional_Arrays/hw-5-4/hw-5-4-2/hw-5-4-2.cpp
@@ -3,12 +3,20 @@ using namespace std;
 
 int main() {
     int rows, columns;
+    int matrix[10][10];
     cout << "Enter the number of rows: ";
     cin >> rows;
+    if (rows > 10 || rows <= 0) {
+        cout << "Max rows of matrix is 10 and above 0" << endl;
+        return 0;
+    }
     cout << "Enter the number of columns: ";
     cin >> columns;
+    if (columns > 10 || columns < 0) {
+        cout << "Max columns of matrix is 10 and above 0" << endl;
+        return 0;
+    }
 
-    int matrix[rows][columns];
     cout << "Enter the elements of the matrix: " << endl;
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
