bf546ea23096d924898a9f60484725fa746a0317
PaulPugovkin <coastalrhodes@gmail.com>
2023-01-25 23:09:21 +0300

    edit homework 5.6.1

--- 5_Two_dimensional_Arrays/hw-5-4/hw-5-4-1/hw-5-4-1.cpp
+++ 5_Two_dimensional_Arrays/hw-5-4/hw-5-4-1/hw-5-4-1.cpp
@@ -3,13 +3,21 @@ using namespace std;
 
 int main() {
     int rows, columns;
+    int matrix[10][10];
     bool sorting = true;
     cout << "Enter the number of rows: ";
     cin >> rows;
+    if (rows > 10 || rows <= 0) {
+        cout << "Max rows of matrix is 10 and above 0" << endl;
+        return 0;
+    }
     cout << "Enter the number of columns: ";
     cin >> columns;
+    if (columns > 10 || columns < 0) {
+        cout << "Max columns of matrix is 10 and above 0" << endl;
+        return 0;
+    }
 
-    int matrix[rows][columns];
     cout << "Enter the elements of the matrix: " << endl;
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < columns; j++) {
@@ -20,7 +28,7 @@ int main() {
     while (sorting) {
         sorting = false;
         for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < columns; j++) {
+            for (int j = 0; j < columns - 1; j++) {
                 if (matrix[i][j] > matrix[i][j+1]) {
                     int boofer = matrix[i][j];
                     matrix[i][j] = matrix[i][j+1];
